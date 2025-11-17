edb50420c89cc42e3c934720470549d84d440f52
uuuumy <e.idanova@g.nsu.ru>
2025-11-17 22:12:00 +0700

    Update 5.4.py

--- 5.4.py
+++ 5.4.py
@@ -3,6 +3,7 @@ if h%10==1 and h!=11:
     print(h,'попугай')
 if (h%10==2 and h!=12) or (h%10==3 and h!=13) or (h%10==4 and h!=14):
     print(h,'попугая')
-if h%10==5 or h%10==6 or h%10==7 or h%10==8 or h%10==9 or h%10==0:
+if h%10==5 or h%10==6 or h%10==7 or h%10==8 or h%10==9 or h%10==0 or h==11 or h==12 or h==13 or h==14:
     print(h, 'попугаев')
+
 
